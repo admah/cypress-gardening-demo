d78d37ce978ea898e7572c17d6bdc3937c28a296
Adam Murray <adamm@cypress.io>
2022-06-19 22:29:12 -0500

    chore: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,10 @@ const Homepage: React.FC = () => {
         <div className="py-10">
           <main>
             <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-              {/* Replace with your content */}
               <div className="px-4 py-8 sm:px-0">
                 <PlantCardList />
                 <Slideover />
               </div>
-              {/* /End replace */}
             </div>
           </main>
         </div>
